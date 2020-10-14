abe560a6f3130681867dab6bd50e4161f9d60740
RENDY FAHMI ROZA <rendyfahmiroza@MacBook-Pro-RENDY.local>
2020-10-14 07:03:22 +0700

    Updated Error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
         config(['app.locale' => 'id']);
         \Carbon\Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
+
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
